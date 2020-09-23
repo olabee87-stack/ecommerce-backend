36fa222c9dce8fe5b9137e5dea658361a4a965e5
Olabisi <53159103+olabee87-stack@users.noreply.github.com>
2020-09-23 17:23:55 +0300

    Use json

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const { errorHandler } = require("../helpers/dbErrorHandler");
 
 //All logics of the userRoutes
 //Exported to the user routes to be called in the routes
@@ -7,10 +8,12 @@ exports.signup = async (req, res) => {
   const user = new User(req.body);
   try {
     await user.save();
-    res.status(201).send(user);
+    res.status(201).json({ user });
     console.log("Saved new user to the db:", user);
   } catch (err) {
-    res.status(400).send(err);
+    res.status(400).json({
+      err: errorHandler(err),
+    });
     console.log("Unable to save user to the DB");
   }
 };
