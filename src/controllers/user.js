b7de8473543290c737400aa98579b9759978cd46
Olabisi <53159103+olabee87-stack@users.noreply.github.com>
2020-09-23 15:49:40 +0300

    Add uuid and crypto for user auth

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,3 +1,5 @@
+const User = require("../models/user");
+
 exports.hiBabe = (req, res) => {
   res.send("How are you");
 };
--- src/models/user.js
+++ src/models/user.js
@@ -1,35 +1,85 @@
 const mongoose = require("mongoose");
-const validator = require("validator");
+const validator = require("validator"); //to add vaidation to schema objects
+const uuid = require("uuid/v1");
+const crypto = require("crypto");
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    validate(value) {
-      if (!validate.isEmail(value)) {
-        throw new Error("Invalid email");
-      }
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 32,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: 32,
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid email");
+        }
+      },
+    },
+    hashed_password: {
+      type: String,
+      required: true,
+      minlength: 7,
+      trim: true,
+      validate(value) {
+        if (value.toLowerCase().includes("password")) {
+          throw new Error(`Password cannot include the word 'password'`);
+        }
+      },
+    },
+    about: {
+      type: String,
+      trim: true,
+    },
+    salt: String,
+    role: {
+      type: Number,
+      default: 0,
+    },
+    history: {
+      type: Array,
+      default: [],
     },
   },
-  password: {
-    type: String,
-    required: true,
-    minlength: 7,
-    trim: true,
-    validate(value) {
-      if (value.toLowerCase().includes("password")) {
-        throw new Error(`Password cannot include the word 'password'`);
-      }
-    },
+  {
+    timestamps: true,
+  }
+);
+
+//@hash password - will be stored virtually, not on the user model
+// hence, the virtual property
+userSchema
+  .virtual("password")
+  .set(function (password) {
+    const user = this;
+
+    user._password = password;
+    user.salt = uuidv1();
+    user._password = user.encryptPassword(password);
+  })
+  .get(function () {
+    return user._password;
+  });
+
+userSchema.methods = {
+  encryptPassword: function (password) {
+    if (!password) return "";
+    try {
+      return crypto
+        .createHmac("sha1", user.salt)
+        .update(password)
+        .digest("hex");
+    } catch (err) {
+      return "";
+    }
   },
-});
+};
 
 const User = new mongoose.model("User", userSchema);
 module.exports = User;
