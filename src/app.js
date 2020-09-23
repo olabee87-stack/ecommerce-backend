88b187b0b911498f2f87b341c2fbe38eaf826fed
Olabisi <53159103+olabee87-stack@users.noreply.github.com>
2020-09-23 13:48:00 +0300

    Add morgan for logging requests

--- src/app.js
+++ src/app.js
@@ -1,7 +1,28 @@
 const express = require("express");
+const morgan = require("morgan");
+const fs = require("fs");
+require("dotenv").config();
 
 const app = express();
 
 //@DB connection
 const connectDB = require("./config/db");
 connectDB();
+
+//@pass incoming json to an object
+app.use(express.json());
+
+//@Morgan for logging http req
+// create a write stream (in append mode)
+const accessLogStream = fs.createWriteStream(__dirname + "/access.log", {
+  flags: "a",
+});
+
+// setup the logger
+app.use(morgan("combined", { stream: accessLogStream }));
+
+//@Listening Port
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
