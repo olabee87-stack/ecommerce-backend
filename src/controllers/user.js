09cdc785c88ae8763169347865b01a778b406475
Olabisi <53159103+olabee87-stack@users.noreply.github.com>
2020-09-24 13:01:13 +0300

    Add logic to for users to log in

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,4 +1,7 @@
 const User = require("../models/user");
+const jwt = require("jsonwebtoken"); //to generate signed token
+const expressJwt = require("express-jwt"); //for authorization check
+
 const { errorHandler } = require("../helpers/dbErrorHandler"); //handle DB error msg
 
 //@All logics of the userRoutes
@@ -17,6 +20,39 @@ exports.signup = async (req, res) => {
     });
     console.log("Unable to save user to the DB");
   }
+};
+
+//@Sign in validator - Log in   //find if user's email exists
+exports.signin = async (req, res) => {
+  const { email, password } = req.body;
+  await User.findOne(
+    {
+      email,
+    },
+    (err, user) => {
+      if (err || !user) {
+        return res.status(400).json({
+          error: "User with that email does not exist, please signup",
+        });
+      }
+
+      //Make sure the email and password matches if the user is found
+      //Create authenticate method in the user model
+      if (!user.authenticate(password)) {
+        return res
+          .status(401)
+          .json({ error: "Email and password do not match" });
+      }
+
+      // const user = this
+      const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET); //auth token - .env
+      //rename token as 't' and include expiry date
+      res.cookie("t", token, { expire: new Date() + 9999 }); //expires in 9999 milliseconds
+      //return response with user and token to the frontend client
+      const { _id, name, email, role } = user;
+      return res.json({ token, user: { _id, email, name, role } });
+    }
+  );
 };
 
 //@Shipped off to the user route..
--- src/models/user.js
+++ src/models/user.js
@@ -39,20 +39,15 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-//@hash password - will be stored virtually, not on the user model
-// hence, the virtual property
-userSchema
-  .virtual("password")
-  .set(function (password) {
-    this._password = password;
-    this.salt = uuidv1();
-    this.hashed_password = this.encryptPassword(password);
-  })
-  .get(function () {
-    return this._password;
-  });
-
+//@encrypt passsword with crypto
+//@authenticate method - called within the user controller when a user is logging in
+//@password accepted at input is compared with the hashed_password (hashed when registering)
+//user can only log in if the password they provide matches the stored one (during registration)
 userSchema.methods = {
+  authenticate: function (password) {
+    return this.encryptPassword(password) === this.hashed_password;
+  },
+
   encryptPassword: function (password) {
     if (!password) return "";
     try {
@@ -66,7 +61,23 @@ userSchema.methods = {
   },
 };
 
+//@Using the encrypted function from up above to hash the passsword
+//@hash password - will be stored virtually, not on the user model
+// hence, the virtual property
+userSchema
+  .virtual("password")
+  .set(function (password) {
+    this._password = password;
+    this.salt = uuidv1();
+    this.hashed_password = this.encryptPassword(password);
+  })
+  .get(function () {
+    return this._password;
+  });
+
 //@Hide hashed password and salt from view
+//@This method will remove the hashed password and the salt string when sent to the client
+//@Logic works on the signup user controller logic before being shipped off to the user route
 userSchema.methods.toJSON = function () {
   const user = this;
   const userObject = user.toObject();
@@ -76,6 +87,8 @@ userSchema.methods.toJSON = function () {
 
   return userObject;
 };
+
+//@Authenticate User before logging in
 
 const User = new mongoose.model("User", userSchema);
 module.exports = User;
