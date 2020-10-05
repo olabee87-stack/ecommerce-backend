2926f7fcf661cb0c087ed19e156765461ac9a9ec
Olabisi <53159103+olabee87-stack@users.noreply.github.com>
2020-10-05 11:35:50 +0300

    Add read and edit func to user route

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,5 +1,6 @@
 const User = require("../models/user");
 
+//@Middleware to find user by ID in the route.param
 exports.userById = async (req, res, next, id) => {
   await User.findById(id).exec((err, user) => {
     if (err || !user) {
@@ -8,6 +9,28 @@ exports.userById = async (req, res, next, id) => {
     req.profile = user; //store the user gotten from the db as req.profile
     next();
   });
+};
+
+//@Read profile - user
+exports.read = async (req, res) => {
+  res.json(req.profile); //the user is available on the req.profile
+};
+
+//@Update - @set - whatever the user inputs to the req.body
+// @new - send the new update back to user
+exports.update = async (req, res) => {
+  try {
+    const user = await User.findOneAndUpdate(
+      { _id: req.profile._id },
+      { $set: req.body },
+      { new: true }
+    );
+    res.json({ user });
+  } catch (err) {
+    res.status(400).json({
+      error: "You are not authorised to perform this action",
+    });
+  }
 };
 
 //"Ship of to userRoutes.js"
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,11 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
-const { userById } = require("../controllers/user");
+const { userById, read, update } = require("../controllers/user");
 
 router.get("/secret/:userId", requireSignin, isAuth, isAdmin, (req, res) => {
   res.json({ user: req.profile });
 });
+
+router.get("/user/:userId", requireSignin, isAuth, read);
+router.put("/user/:userId", requireSignin, isAuth, update);
 
 //run the userById method anytime there is a user's Id parameter in the route
 //Makes user info (profile) available in the req object
