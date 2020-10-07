ebad5a79a61b02420e36bda7188a29cf59f02189
Olabisi <53159103+olabee87-stack@users.noreply.github.com>
2020-10-07 15:10:11 +0300

    Add order model to order controller

--- src/controllers/order.js
+++ src/controllers/order.js
@@ -1,8 +1,19 @@
 import { Order, CartItem } from "../models/order";
 import { errorHandler } from "../helpers/dbErrorHandler";
-//const { Order, CartItem } = require("../models/order");
 
 //@Show what is sent from the frontend to create a new order
-export function create(req, res) {
-  console.log("CREATE ORDER: ", req.body);
-}
+exports.create = async (req, res) => {
+  //console.log("CREATE ORDER: ", req.body);
+  //@get user before saving order - @req-profile coming from the user db
+  req.body.order.user = req.profile;
+  const order = await new Order(req.body.order); //@whatever order the user places on the client
+
+  try {
+    const data = order.save();
+    res.json(data);
+  } catch (error) {
+    return res.status(400).json({
+      error: errorHandler(error),
+    });
+  }
+};
