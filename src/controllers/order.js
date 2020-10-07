12bb2237d37107fdccf2b9376b2bfddd07500ac9
Olabisi <53159103+olabee87-stack@users.noreply.github.com>
2020-10-07 15:28:17 +0300

    Edit

--- src/controllers/order.js
+++ src/controllers/order.js
@@ -3,7 +3,6 @@ import { errorHandler } from "../helpers/dbErrorHandler";
 
 //@Show what is sent from the frontend to create a new order
 exports.create = async (req, res) => {
-  //console.log("CREATE ORDER: ", req.body);
   //@get user before saving order - @req-profile coming from the user db
   req.body.order.user = req.profile;
   const order = await new Order(req.body.order); //@whatever order the user places on the client
@@ -11,6 +10,7 @@ exports.create = async (req, res) => {
   try {
     const data = order.save();
     res.json(data);
+    console.log("CREATED NEW ORDER: ", data);
   } catch (error) {
     return res.status(400).json({
       error: errorHandler(error),
